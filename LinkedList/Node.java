8d792951ec9b3ea1f215b89fcf7e0175b6400155
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-23 23:26:39 +0530

    Creating simple linkedlist of 56,30&70

---
+++ LinkedList/Node.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class Node {
+	int key;
+	Node next;
+	public int getKey() {
+		return key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
